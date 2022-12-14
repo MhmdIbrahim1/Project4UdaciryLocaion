3942382eb74b4d373cc02263c8e8613620361358
MhmdIbrahim1 <S2051710242EG@std.aou.edu.eg>
2022-12-14 23:51:14 +0200

    Initial commit

---
+++ app/src/main/java/com/udacity/project4/locationreminders/geofence/GeofenceTransitionsJobIntentService.kt
@@ -0,0 +1,81 @@
+package com.udacity.project4.locationreminders.geofence
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import androidx.core.app.JobIntentService
+import com.udacity.project4.locationreminders.data.ReminderDataSource
+import com.udacity.project4.locationreminders.data.dto.ReminderDTO
+import com.udacity.project4.locationreminders.data.dto.Result
+import com.udacity.project4.locationreminders.reminderslist.ReminderDataItem
+import com.udacity.project4.utils.sendNotification
+import com.google.android.gms.location.Geofence
+import com.google.android.gms.location.GeofencingEvent
+import kotlinx.coroutines.*
+import org.koin.android.ext.android.inject
+import kotlin.coroutines.CoroutineContext
+
+class GeofenceTransitionsJobIntentService : JobIntentService(), CoroutineScope {
+
+    private val remindersLocalRepository: ReminderDataSource by inject()
+    private val TAG = "GeofenceTransitionsJobIntentService"
+    private var coroutineJob: Job = Job()
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.IO + coroutineJob
+
+    companion object {
+        private const val JOB_ID = 573
+        fun enqueueWork(context: Context, intent: Intent) {
+            enqueueWork(
+                context,
+                GeofenceTransitionsJobIntentService::class.java, JOB_ID,
+                intent
+            )
+        }
+    }
+
+    @SuppressLint("LongLogTag")
+    override fun onHandleWork(intent: Intent) {
+        val geofencingEvent = GeofencingEvent.fromIntent(intent)
+        if (geofencingEvent.hasError()) {
+            val errorMessage = geofencingEvent.errorCode
+            Log.e(TAG, errorMessage.toString())
+            return
+        }
+        if (geofencingEvent.geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER || geofencingEvent.geofenceTransition ==Geofence.GEOFENCE_TRANSITION_DWELL) {
+            Log.e(TAG, "GEOFENCE TRANSITION ENTER")
+            sendNotification(geofencingEvent.triggeringGeofences)
+        }
+
+    }
+    //TODO: get the request id of the current geofence
+    private fun sendNotification(triggeringGeofences: List<Geofence>) {
+        triggeringGeofences.forEach {
+            val requestId = it.requestId
+            //Get the local repository instance
+            val remindersLocalRepository: ReminderDataSource by inject()
+//        Interaction to the repository has to be through a coroutine scope
+            CoroutineScope(coroutineContext).launch(SupervisorJob()) {
+                //get the reminder with the request id
+                val result = remindersLocalRepository.getReminder(requestId)
+                if (result is Result.Success<ReminderDTO>) {
+                    val reminderDTO = result.data
+                    //send a notification to the user with the reminder details
+                    sendNotification(
+                        this@GeofenceTransitionsJobIntentService, ReminderDataItem(
+                            reminderDTO.title,
+                            reminderDTO.description,
+                            reminderDTO.location,
+                            reminderDTO.latitude,
+                            reminderDTO.longitude,
+                            reminderDTO.id
+                        )
+                    )
+                }
+            }
+
+        }
+
+    }
+}
